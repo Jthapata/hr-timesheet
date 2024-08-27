702d712a1c857e47047b7e95262d3a40c495a0ab
jthapata <jessezap01@gmail.com>
2024-08-27 10:06:32 -0600

    setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './modules/app-routing.module';
 import { AppComponent } from './app.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 @NgModule({
-  declarations: [
+  declarations: [ //components associated w this module
     AppComponent
   ],
-  imports: [
+  imports: [ //all of our imports available to our components
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [ //provice services to our components
+    provideAnimationsAsync()
+  ],
+  bootstrap: [AppComponent] //first component that loads
 })
 export class AppModule { }
